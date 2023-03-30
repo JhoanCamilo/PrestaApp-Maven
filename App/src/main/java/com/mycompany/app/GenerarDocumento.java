0c5b7a9d94f82dd7778e94660db6efa5451fcb1a
Jhoan Camilo Arango Monsalve <51723353+JhoanCamilo@users.noreply.github.com>
2023-03-30 08:57:08 -0500

    Add files via upload

--- App/src/main/java/com/mycompany/app/GenerarDocumento.java
+++ App/src/main/java/com/mycompany/app/GenerarDocumento.java
@@ -6,12 +6,10 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
-/*import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.util.JRLoader;
-import net.sf.jasperreports.view.JasperViewer;*/
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.PdfPTable;
 
 
 
@@ -23,25 +21,9 @@ public class GenerarDocumento {
         Conexion funciones = new Conexion();
         
         void GenerarReporte() {
-                 funciones.ValidarDriver();
-                 Connection conn;
-                   
-                /*try {
-                    conn = DriverManager.getConnection(URL, Usuario, Clave);
-                    JasperReport reporte = null;
-                    String path = "src\\Reportes\\report1.jasper";
-                     try {
-                         reporte = (JasperReport) JRLoader.loadObjectFromFile(path);
-                         JasperPrint jprint = JasperFillManager.fillReport(reporte, null, conn);
-                         JasperViewer vista = new JasperViewer(jprint, false);
+            funciones.ValidarDriver();
+            Connection conn;
 
-                        vista.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-                        vista.setVisible(true);
-                     } catch (JRException ex) {
-                         Logger.getLogger(GenerarDocumento.class.getName()).log(Level.SEVERE, null, ex);
-                     }
-                } catch (SQLException ex) {
-                    java.util.logging.Logger.getLogger(GenerarDocumento.class.getName()).log(Level.SEVERE, null, ex);
-                }*/
+
         }
 }
